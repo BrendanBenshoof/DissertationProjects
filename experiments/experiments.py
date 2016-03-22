39abc83c66d4c47fa3d2f8bb9c61371278be8a06
= <brendan.benshoof@gmail.com>
2016-03-22 08:49:16 -0400

    check out the json

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -1,3 +1,5 @@
+#!/home/brendan/anaconda3/bin/python3
+
 import networkx as nx
 import json
 import random
@@ -13,35 +15,43 @@ def underlay_failure(g, r):
     g.remove_nodes_from(to_remove)
 
 
-def overlay_failure(g, r):
-    pass
+def overlay_failure(g,dists, r):
+    partial = len(g.nodes()) * r
+    # print(partial)
+    num = int(partial)
+    target = random.choice(g.nodes())
+    to_remove = sorted(g.nodes(), key=lambda x: dists[target][x]+random.random()*0.5)[:num]
+    g.remove_nodes_from(to_remove)
 
-
-def random_k(g, k):
+def random_k(g,dists, k):
     return random.sample(g.nodes(), k)
 
 
-def nearest_k(g, k):
+def nearest_k(g,dists, k):
     target = random.choice(g.nodes())
-    return [target] + sorted(g.nodes(), key=lambda x: nx.shortest_path_length(g, target, x))[1:k + 1]
+    return [target] + sorted(g.nodes(), key=lambda x: dists[x][target]+random.random()*0.5)[1:k + 1]
 
 
-def runtests():
-    underlay_data_failure = {"title": "Underlay Failure Likelihood",
+def runtests_underlay():
+    underlay_data_failure = {"title": "O Failure Likelihood",
                              "x-axis": "Percentage of nodes removed", "y-axis": "Liklihood of data loss",
                              "plots": {}}
 
     underlay_data_partition = {"title": "Underlay Partition Chance",
                                "x-axis": "Percentage of nodes removed", "y-axis": "Liklihood of Partition",
                                "plots": {}}
+    g_chord = nx.read_gpickle("chord.pickle")
+    chord_paths = nx.all_pairs_shortest_path_length(g_chord)
+    g_kad = nx.read_gpickle("kad.pickle")
+    kad_paths = nx.all_pairs_shortest_path_length(g_kad)
 
-    graphs = {"Chord": "chord.pickle", "Kademlia": "kad.pickle"}
+    graphs = {"Chord":(g_chord, chord_paths) , "Kademlia":(g_kad,kad_paths) }
     strats = {"Random K": random_k, "Nearest K": nearest_k}
     K = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     R = [0.1 + x * 0.1 for x in range(9)]
     samples = 100
 
-    for graph_name, graph_file in graphs.items():
+    for graph_name, graph_data in graphs.items():
         for strats_name, strats_func in strats.items():
             for k in K:
                 xs = R[:]
@@ -52,8 +62,9 @@ def runtests():
                     survivor_total = 0.0
                     partition_total = 0.0
                     for null in range(samples):
-                        g = nx.read_gpickle(graph_file)
-                        replicas = strats_func(g, k)
+                        g = graph_data[0].copy()
+                        dists = graph_data[1]
+                        replicas = strats_func(g,dists, k)
                         underlay_failure(g, r_)
                         if nx.is_strongly_connected(g):
                             partition_total += 1.0
@@ -80,4 +91,52 @@ def runtests():
     with open("Underlay_survival.json", "w+") as fp:
         json.dump(underlay_data_failure, fp)
 
-runtests()
+def runtests_overlay():
+    underlay_data_failure = {"title": "Overlay Failure Likelihood",
+                             "x-axis": "Percentage of nodes removed", "y-axis": "Liklihood of data loss",
+                             "plots": {}}
+
+    g_chord = nx.read_gpickle("chord.pickle")
+    chord_paths = nx.all_pairs_shortest_path_length(g_chord)
+    g_kad = nx.read_gpickle("kad.pickle")
+    kad_paths = nx.all_pairs_shortest_path_length(g_kad)
+
+    graphs = {"Chord":(g_chord, chord_paths) , "Kademlia":(g_kad,kad_paths) }
+    strats = {"Random K": random_k, "Nearest K": nearest_k}
+    K = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    R = [0.1 + x * 0.1 for x in range(9)]
+    samples = 100
+
+    for graph_name, graph_data in graphs.items():
+        for strats_name, strats_func in strats.items():
+            for k in K:
+                xs = R[:]
+                # print(xs)
+                failure_likelihoods = []
+                for r_ in xs:
+                    survivor_total = 0.0
+                    partition_total = 0.0
+                    for null in range(samples):
+                        g = graph_data[0].copy()
+                        dists = graph_data[1]
+                        replicas = strats_func(g,dists, k)
+                        overlay_failure(g,dists, r_)
+
+                        victory = False
+                        for r in replicas:
+                            if r in g.nodes():
+                                victory = True
+                        if not victory:
+                            survivor_total += 1.0
+                    survivor_total /= samples
+                    partition_total /= samples
+                    failure_likelihoods.append(survivor_total)
+                failure_plot = {"x": xs, "y": failure_likelihoods}
+                print("%s %s-%d" % (graph_name, strats_name, k))
+                underlay_data_failure["plots"]["%s %s-%d Loss Chance" %
+                                               (graph_name, strats_name, k)] = failure_plot
+
+    with open("Overlay_survival.json", "w+") as fp:
+        json.dump(underlay_data_failure, fp)
+
+runtests_overlay()
